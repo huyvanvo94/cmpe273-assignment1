e5a48f2c977407d1dc9bf305d49c476f4542cb23
huyvanvo94 <huyvanvo94@gmail.com>
2018-09-28 03:33:05 -0700

    save work

--- main.py
+++ main.py
@@ -1,34 +1,5 @@
-def call_counter(func):
-    def helper(x):
-        helper.calls += 1
-        return func(x)
+import yaml
+with open('config.yaml', 'r') as f:
+    doc = yaml.load(f)
 
-    helper.calls = 0
-
-    return helper
-
-
-@call_counter
-def succ(x):
-    return x + 1
-
-
-print(succ.calls)
-for i in range(10):
-    succ(i)
-
-print(succ.calls)
-
-
-@call_counter
-def mul1(x, y=1):
-    return x * y + 1
-
-print( succ(10).helper.calls)
-class Foo:
-    def __init__(self): pass
-
-    @call_counter
-    def succ(self, x):
-        return x + 1
-
+    print(doc)
